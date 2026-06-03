990165f550d9218c674cb7c04e826ccbe803d040
maria <maria@Andrielly>
2026-06-03 18:39:36 -0300

    alterei o main

--- src/hotelUepb/Main.java
+++ src/hotelUepb/Main.java
@@ -1,10 +1,17 @@
 package hotelUepb;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// jtdddddddddd7
-
+		
+		// kkkkkkkkk
+		
+		Scanner leitor = new Scanner (System.in);
+		
+		leitor.close();
 	}
 
 }
