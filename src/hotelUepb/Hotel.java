0b7c8707b9cd951e871839ad2e6c9d4be40532be
fabby <fabby@Fabola>
2026-06-04 14:38:42 -0300

    mudei o hotel

--- src/hotelUepb/Hotel.java
+++ src/hotelUepb/Hotel.java
@@ -1,5 +1,32 @@
 package hotelUepb;
 
-public class Hotel {
+public class Hotel 
+{
+	String nomeHotel;
+	double valorDiaria;
+	Reserva reservasAtivas[];
+	int quantidadeAtual;
+	
+	Hotel(String nomeHotel, double valorDiaria, int capacidadeMaxima)
+	{
+		this.valorDiaria = valorDiaria;
+		this.nomeHotel = nomeHotel;
+		this.reservasAtivas = new Reserva[capacidadeMaxima];
+		this.quantidadeAtual = 0;
+	}
+	
+	boolean cadastrarReserva(Reserva novaReserva)
+	{
 
+		if(quantidadeAtual >= reservasAtivas.length)
+		{
+			System.out.println("Sem quartos disponíveis, impossível cadastrar novo hóspede.");
+			return false;
+		}
+		
+		reservasAtivas[quantidadeAtual] = novaReserva;
+		quantidadeAtual++;
+		return true;
+	}
+	
 }
