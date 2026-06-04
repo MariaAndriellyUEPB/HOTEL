d1baaa565c004ba20c756f34972039838908d73d
fabby <fabby@Fabola>
2026-06-04 15:34:45 -0300

    pagamento

--- src/hotelUepb/Reserva.java
+++ src/hotelUepb/Reserva.java
@@ -10,11 +10,12 @@ public class Reserva {
 	double valorDiaria;
 	
 	
-	Reserva(String codigo, String tipoQuarto, String numeroQuarto, String nomeHospede, int quantidadeDias, double valorDiaria){
+	Reserva(String codigo, String tipoQuarto, String numeroQuarto, String nomeHospede, 	String formaDePagamento, int quantidadeDias, double valorDiaria){
 		this.codigo = codigo;
 		this.tipoQuarto = tipoQuarto;
 		this.numeroQuarto = numeroQuarto;
 		this.nomeHospede = nomeHospede;
+		this.formaDePagamento = formaDePagamento;
 		this.quantidadeDias = quantidadeDias;
 		this.valorDiaria = valorDiaria;
 		
