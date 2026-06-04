bc5a69c2791866ecc9a244ed505d48991c32d6cb
Pessoal <Pessoal@DESKTOP-TNQJ17O>
2026-06-04 15:39:52 -0300

    dbd

--- src/hotelUepb/Reserva.java
+++ src/hotelUepb/Reserva.java
@@ -9,20 +9,19 @@ public class Reserva {
 	int quantidadeDias;
 	double valorDiaria;
 	
-	
-	Reserva(String codigo, String tipoQuarto, String numeroQuarto, String nomeHospede, 	String formaDePagamento, int quantidadeDias, double valorDiaria){
+	Reserva(String codigo, String tipoQuarto, String numeroQuarto, String nomeHospede, int quantidadeDias, double valorDiaria){
 		this.codigo = codigo;
 		this.tipoQuarto = tipoQuarto;
 		this.numeroQuarto = numeroQuarto;
 		this.nomeHospede = nomeHospede;
-		this.formaDePagamento = formaDePagamento;
 		this.quantidadeDias = quantidadeDias;
 		this.valorDiaria = valorDiaria;
-		
 	}
+	
 	double calcularDiariaTotal() {
 		return valorDiaria*quantidadeDias;
 	}
+	
 	public String toString() {
 		return "---Reserva---" + "\n---Dados do Hóspedes---" + 
 	"\nCódigo:  " + codigo + 
