982de659631b91cb45485cfdfe5f24a430de988a
fabby <fabby@Fabola>
2026-06-04 15:09:21 -0300

    fiz as opções

--- src/hotelUepb/Hotel.java
+++ src/hotelUepb/Hotel.java
@@ -1,5 +1,7 @@
 package hotelUepb;
 
+import Hotel.Reserva;
+
 public class Hotel 
 {
 	String nomeHotel;
@@ -43,6 +45,59 @@ public class Hotel
 		}
 		
 		return false;
+	}
+	
+	Reserva[] buscarReservasPorHospede(String nomeDoHospede)
+	{
+		if(quantidadeAtual == 0)
+		{
+			System.out.println("Nenhum hóspede cadastrado.");
+			return null;
+		}
+		
+		int somaReservas = 0;
+		
+		for(int i = 0; i < quantidadeAtual; i++)
+		{
+			if(reservasAtivas[i].nomeHospede.equalsIgnoreCase(nomeDoHospede))
+			{
+				somaReservas++;
+			}
+		}
+		
+		if(somaReservas == 0)
+		{
+			System.out.println("Nenhuma reserva com o nome de '" + nomeDoHospede + "' foi encontrada.");
+			return null;
+		}
+		
+		Reserva auxiliar[] = new Reserva[somaReservas];
+		
+		int j = 0;
+		for(int i = 0; i < quantidadeAtual; i++)
+		{
+			if(reservasAtivas[i].nomeHospede.equalsIgnoreCase(nomeDoHospede))
+			{
+				auxiliar[j] = reservasAtivas[i];
+				j++;
+			}
+		}
+
+		return auxiliar;
+	}
+	
+	void exibirRelatorioDeReservas()
+	{
+		if(quantidadeAtual == 0)
+		{
+			System.out.println("Nenhum hóspede cadastrado.");
+			return;
+		}
+		
+		for(int i = 0; i < quantidadeAtual; i++)
+		{
+			System.out.println(reservasAtivas[i]);
+		}
 	}
 	
 }
