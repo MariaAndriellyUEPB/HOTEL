72bfdbb11fe67d5836bdbb06a1e9db2254fa106e
fabby <fabby@Fabola>
2026-06-04 16:04:29 -0300

    to String

--- src/hotelUepb/Reserva.java
+++ src/hotelUepb/Reserva.java
@@ -9,7 +9,8 @@ public class Reserva {
 	int quantidadeDias;
 	double valorDiaria;
 	
-	Reserva(String codigo, String tipoQuarto, String numeroQuarto, String nomeHospede, String formaDePagamento, int quantidadeDias, double valorDiaria){
+	
+	Reserva(String codigo, String tipoQuarto, String numeroQuarto, String nomeHospede, 	String formaDePagamento, int quantidadeDias, double valorDiaria){
 		this.codigo = codigo;
 		this.tipoQuarto = tipoQuarto;
 		this.numeroQuarto = numeroQuarto;
@@ -17,12 +18,11 @@ public class Reserva {
 		this.formaDePagamento = formaDePagamento;
 		this.quantidadeDias = quantidadeDias;
 		this.valorDiaria = valorDiaria;
+		
 	}
-	
 	double calcularDiariaTotal() {
 		return valorDiaria*quantidadeDias;
 	}
-	
 	public String toString() {
 		return "---Reserva---" + "\n---Dados do Hóspedes---" + 
 	"\nCódigo:  " + codigo + 
@@ -33,5 +33,4 @@ public class Reserva {
 	"\nNúmero do quarto: " + numeroQuarto + 
 	"\nValor da diária: " + calcularDiariaTotal()   ; 
 	}
-
 }
