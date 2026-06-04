a82a26390dcf288d78bed89aa33d3977df534c6b
fabby <fabby@Fabola>
2026-06-04 16:19:02 -0300

    iu

--- src/hotelUepb/Hotel.java
+++ src/hotelUepb/Hotel.java
@@ -14,10 +14,6 @@ public class Hotel
 	}
 	
 	boolean cadastrarReserva(Reserva novaReserva){
-			if(quantidadeAtual >= reservasAtivas.length){
-			System.out.println("Sem quartos disponíveis, impossível cadastrar novo hóspede.");
-			return false;
-		}
 		reservasAtivas[quantidadeAtual] = novaReserva;
 		quantidadeAtual++;
 		return true;
