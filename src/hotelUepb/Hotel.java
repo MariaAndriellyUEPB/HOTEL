2f32d6eeda0439103670255f05401a68c45c6e91
fabby <fabby@Fabola>
2026-06-04 15:11:38 -0300

    oi

--- src/hotelUepb/Hotel.java
+++ src/hotelUepb/Hotel.java
@@ -1,7 +1,5 @@
 package hotelUepb;
 
-import Hotel.Reserva;
-
 public class Hotel 
 {
 	String nomeHotel;
@@ -9,23 +7,18 @@ public class Hotel
 	Reserva reservasAtivas[];
 	int quantidadeAtual;
 	
-	Hotel(String nomeHotel, double valorDiaria, int capacidadeMaxima)
-	{
+	Hotel(String nomeHotel, double valorDiaria, int capacidadeMaxima){	
 		this.valorDiaria = valorDiaria;
 		this.nomeHotel = nomeHotel;
 		this.reservasAtivas = new Reserva[capacidadeMaxima];
 		this.quantidadeAtual = 0;
 	}
 	
-	boolean cadastrarReserva(Reserva novaReserva)
-	{
-
-		if(quantidadeAtual >= reservasAtivas.length)
-		{
+	boolean cadastrarReserva(Reserva novaReserva){
+			if(quantidadeAtual >= reservasAtivas.length){
 			System.out.println("Sem quartos disponíveis, impossível cadastrar novo hóspede.");
 			return false;
 		}
-		
 		reservasAtivas[quantidadeAtual] = novaReserva;
 		quantidadeAtual++;
 		return true;
