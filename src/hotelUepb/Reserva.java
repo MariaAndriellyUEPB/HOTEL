1e3ffd668967d2d090ad960c0cadaf4c783ac3a8
maria <maria@Andrielly>
2026-07-18 11:03:31 -0300

    get e validar dados na classe reserva

--- src/hotelUepb/Reserva.java
+++ src/hotelUepb/Reserva.java
@@ -8,30 +8,64 @@ public class Reserva {
 	private String formaDePagamento;
 	private int quantidadeDias;
 	private double valorDiaria;
-	
-	
-	public Reserva(String codigo, String tipoQuarto, String numeroQuarto, String nomeHospede, 	String formaDePagamento, int quantidadeDias, double valorDiaria){
+
+	public Reserva(String codigo, String tipoQuarto, String numeroQuarto, String nomeHospede, String formaDePagamento,
+			int quantidadeDias, double valorDiaria) {
 		this.codigo = codigo;
 		this.tipoQuarto = tipoQuarto;
 		this.numeroQuarto = numeroQuarto;
 		this.nomeHospede = nomeHospede;
 		this.formaDePagamento = formaDePagamento;
 		this.quantidadeDias = quantidadeDias;
-		this.valorDiaria = valorDiaria;
-		
+		this.valorDiaria = validarDados(valorDiaria);
+
 	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public String getTipoQuarto() {
+		return tipoQuarto;
+	}
+
+	public String getNumeroQuarto() {
+		return numeroQuarto;
+	}
+
+	public String getNomeHospede() {
+		return nomeHospede;
+	}
+
+	public String getFormaDePagamento() {
+		return formaDePagamento;
+	}
+
+	public int getQuantidadeDias() {
+		return quantidadeDias;
+	}
+
+	public double getValorDiaria() {
+		return valorDiaria;
+	}
+	
+	private double validarDados(double valor) {
+		if(valor > 0) {
+			return valor;
+		}
+		return 0.0;
+	}
+
 	public double calcularDiariaTotal() {
-		return valorDiaria*quantidadeDias;
+		return valorDiaria * quantidadeDias;
 	}
+
 	public String toString() {
-		return "\n================================================================\n--- Dados do Hóspedes ---" +
-	"\n Código:  " + codigo +
-	"\n Nome do hóspede:  " + nomeHospede +
-	"\n Forma de pagamento: " + formaDePagamento +
-	"\n Quantidades de dias: " + quantidadeDias +
-	"\n\n---Dados do Quarto---" + "\n Tipo do Quarto: " + tipoQuarto +
-	"\n Número do quarto: " + numeroQuarto +
-	"\n Valor da diária: R$ " + valorDiaria +
-	"\n Total a pagar: R$ " + calcularDiariaTotal() + "\n================================================================";
+		return "\n================================================================\n--- Dados do Hóspedes ---"
+				+ "\n Código:  " + codigo + "\n Nome do hóspede:  " + nomeHospede + "\n Forma de pagamento: "
+				+ formaDePagamento + "\n Quantidades de dias: " + quantidadeDias + "\n\n---Dados do Quarto---"
+				+ "\n Tipo do Quarto: " + tipoQuarto + "\n Número do quarto: " + numeroQuarto
+				+ "\n Valor da diária: R$ " + valorDiaria + "\n Total a pagar: R$ " + calcularDiariaTotal()
+				+ "\n================================================================";
 	}
 }
