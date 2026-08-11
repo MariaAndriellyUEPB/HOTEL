96bf1c316125a4b7fddcab29d93019bac23a7458
fabby <fabby@Fabola>
2026-08-11 12:52:02 -0300

    validador

--- src/classesDeQuartos/Quarto.java
+++ src/classesDeQuartos/Quarto.java
@@ -1,11 +1,15 @@
 package classesDeQuartos;
 
+import validador.Validador;
+
 public abstract class Quarto {  
 	private String nomeDoQuarto;
 	private String numeroQuarto;
 	private double valorDiaria;
 
-	public Quarto(String nomeDoQuarto, String numeroQuarto, double valorDiaria) {
+	public Quarto(String nomeDoQuarto, String numeroQuarto, double valorDiaria) throws Exception {
+		Validador.validaValorMenorQueZero(valorDiaria, "valor da diaria invalido.");
+		
 		this.nomeDoQuarto = nomeDoQuarto;
 		this.numeroQuarto = numeroQuarto;
 		this.valorDiaria = valorDiaria;
